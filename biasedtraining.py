c19cfe09d8200e90ce880bd9b173f41fe7ae9017
Javier <javier@pop-os.localdomain>
2021-04-15 16:38:09 +0200

    Finished lab 4.1 and 4.2

--- biasedtraining.py
+++ biasedtraining.py
@@ -1,4 +1,6 @@
 #Train 3 gender classifiers using only subjects from the same race (one classifier per race)
+
+#%% Imports
 from dataloader import DiveFaceDataLoader
 import keras
 import os
@@ -21,21 +23,22 @@ from tensorflow.python.keras.backend import ndim
 import tensorflow as tf
 from tensorflow.compat.v1 import InteractiveSession
 
-## To solve some GPU DRAM limitations
+#%%  To solve some GPU DRAM limitations
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
+#%% Load dataset
 #load whole dataset
 demo_data = DiveFaceDataLoader().LoadData("4K_120")
 #Set UP so its usable with keras ImageDataGenerator
 demo_data.rename(columns={'Image':'filename'},inplace=True)
 
-#Load VGGFace with resnet50 backbone
+#%% Load VGGFace with resnet50 backbone
 my_model = 'resnet50'
 resnet = VGGFace(model = my_model)
 
-#Select the lat leayer as feature embedding  
+#Select the last leayer as feature embedding  
 last_layer = resnet.get_layer('avg_pool').output
 feature_layer = Flatten(name='flatten')(last_layer)
 model_vgg=Model(resnet.input, feature_layer)
@@ -43,6 +46,7 @@ model_vgg=Model(resnet.input, feature_layer)
 #Freeze the model
 model_vgg.trainable = False
 
+#%% Generate subsets 
 #Create the 3 subsets from the dataset (one per race)
 #white people (about 55k images keep one per identity)
 white_entries = demo_data.drop(['HN','HA','MN','MA'],axis=1)
@@ -55,49 +59,103 @@ asian_entries = asian_entries[demo_data['HA'] != demo_data['MA']]
 afr_entries = demo_data.drop(['HA','HB','MB','MA'],axis=1)
 afr_entries = afr_entries[demo_data['HN'] != demo_data['MN']]
 
-#Train a classifier for gender using these datasets
-white_gender_class = keras.Sequential([
-    model_vgg,
-    keras.layers.Dense(3000,activation="relu"),
-    keras.layers.Dense(2,activation="softmax")]
-)
-white_gender_class.compile(loss='categorical_crossentropy',metrics=['acc'])
+#%% Create the training and testing ImageDataGenerators
 
-#Preprocess the images
+#Preprocessing used for the images
 def preprocess(img):
     img = np.expand_dims(img, axis=0)
     return img
 
 #Split into training and validation
 training_split  = 0.8
+
+## White people
 indxs = np.random.rand(len(white_entries)) < training_split 
 training = white_entries[indxs]
 print("Training entries",len(training))
-training_data = ImageDataGenerator(preprocessing_function=preprocess).flow_from_dataframe(training,directory=".",target_size=(224,224),y_col=['HB','MB'],class_mode='raw')
+training_data_white = ImageDataGenerator(preprocessing_function=preprocess).flow_from_dataframe(training,directory=".",target_size=(224,224),y_col=['HB','MB'],class_mode='raw')
 testing = white_entries[~indxs]
 print("Testing entries",len(testing))
-testing_data = ImageDataGenerator(preprocessing_function=preprocess).flow_from_dataframe(testing,directory=".",target_size=(224,224),y_col=['HB','MB'],class_mode='raw')
+testing_data_white = ImageDataGenerator(preprocessing_function=preprocess).flow_from_dataframe(testing,directory=".",target_size=(224,224),y_col=['HB','MB'],class_mode='raw')
+
+## Black people
+indxs = np.random.rand(len(afr_entries)) < training_split 
+training = afr_entries[indxs]
+print("Training entries",len(training))
+training_data_black = ImageDataGenerator(preprocessing_function=preprocess).flow_from_dataframe(training,directory=".",target_size=(224,224),y_col=['HN','MN'],class_mode='raw')
+testing = afr_entries[~indxs]
+print("Testing entries",len(testing))
+testing_data_black= ImageDataGenerator(preprocessing_function=preprocess).flow_from_dataframe(testing,directory=".",target_size=(224,224),y_col=['HN','MN'],class_mode='raw')
+
+## Asian people
+indxs = np.random.rand(len(asian_entries)) < training_split 
+training = asian_entries[indxs]
+print("Training entries",len(training))
+training_data_asian= ImageDataGenerator(preprocessing_function=preprocess).flow_from_dataframe(training,directory=".",target_size=(224,224),y_col=['HA','MA'],class_mode='raw')
+testing = asian_entries[~indxs]
+print("Testing entries",len(testing))
+testing_data_asian = ImageDataGenerator(preprocessing_function=preprocess).flow_from_dataframe(testing,directory=".",target_size=(224,224),y_col=['HA','MA'],class_mode='raw')
 
 
-wg_checkpoint_filepath = 'model/checkpoints/wg_c'
-model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=wg_checkpoint_filepath,
-    save_weights_only=True,
-    monitor='val_acc',
-    mode='max',
-    save_best_only=True)
+#%% White people  create classifier model 
 
-train = True
-if train:
-    white_gender_class.fit(training_data,validation_data=testing_data,
-                               epochs=3,
-                               callbacks=[model_checkpoint_callback])
+white_gender_class = keras.Sequential([
+    model_vgg,
+    keras.layers.Dense(3000,activation="relu"),
+    keras.layers.Dense(2,activation="softmax")]
+)
+white_gender_class.compile(loss='categorical_crossentropy',metrics=['acc'])
+#%% Train a classifier for gender using these datasets
+white_gender_class.fit(training_data_white,validation_data=testing_data_white,
+                               epochs=3)
+
+#%% Evaluate on other races
+#Black
+print("Accuracy over Black demographic group")
+white_gender_class.evaluate(testing_data_black)
+
+#Asian
+print("Accuracy over Asian demographic group")
+white_gender_class.evaluate(testing_data_asian)
 
 
+#%% Black people create classifier model
+black_gender_class = keras.Sequential([
+    model_vgg,
+    keras.layers.Dense(3000,activation="relu"),
+    keras.layers.Dense(2,activation="softmax")]
+)
+black_gender_class.compile(loss='categorical_crossentropy',metrics=['acc'])
+#%% Train a classifier for gender using these datasets
+black_gender_class.fit(training_data_black,validation_data=testing_data_black,
+                               epochs=3)
 
+#%% Evaluate on other races
+#Black
+print("Accuracy over White demographic group")
+black_gender_class.evaluate(testing_data_white)
 
+#Asian
+print("Accuracy over Asian demographic group")
+black_gender_class.evaluate(testing_data_asian)
 
+#%% Asian people create classifier model
+asian_gender_class = keras.Sequential([
+    model_vgg,
+    keras.layers.Dense(3000,activation="relu"),
+    keras.layers.Dense(2,activation="softmax")]
+)
+asian_gender_class.compile(loss='categorical_crossentropy',metrics=['acc'])
+#%% Train a classifier for gender using these datasets
+asian_gender_class.fit(training_data_asian,validation_data=testing_data_asian,
+                               epochs=3)
 
+#%% Evaluate on other races
+#Black
+print("Accuracy over Black demographic group")
+asian_gender_class.evaluate(testing_data_black)
 
-
-
+#Asian
+print("Accuracy over White demographic group")
+asian_gender_class.evaluate(testing_data_white)
+# %%
